f112d9543fd230c93a828a0f68dfa07d2cf8ba0a
Blake Redwolf <blakethedead@gmail.com>
2017-07-05 14:24:16 -0800

    Index page animations added

---
+++ assets/js/index.js
@@ -0,0 +1,115 @@
+$( document ).ready(function() {
+
+    // For on scroll effect
+    $(window).scroll(function() {
+		$('#fa-float').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+1000) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-float2').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+900) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-float3').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+800) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-float4').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+700) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-float5').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+600) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-float6').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+600) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-foot').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+1000) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-foot2').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+800) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    $(window).scroll(function() {
+		$('#fa-foot3').each(function(){ 
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+700) {
+				$(this).addClass("fadeIn");
+			}
+		});
+	});
+
+    // For on click effect
+    $('#aboutbtn').click(function() {
+		$(this).addClass("expandOpen");
+	});
+
+    $('#').click(function() {
+        $(this).addClass("");
+    });
+
+});
+
+// Replace #animatedElement with the ID or class of the element you want animated.
+// Replace slideUp with an animation class
